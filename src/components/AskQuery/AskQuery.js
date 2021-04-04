1850bed7001901ca6e5b598e44f02804d7dff0f2
Bhasker Goel <bhasker2000goel@gmail.com>
2021-04-05 02:20:02 +0530

    Minor changes

--- src/components/AskQuery/AskQuery.js
+++ src/components/AskQuery/AskQuery.js
@@ -1,100 +1,124 @@
-import React,{useState} from 'react';
-import classes from './AskQuery.module.css';
+import React, { useState } from "react";
+import classes from "./AskQuery.module.css";
 
 function ValidationMessage(props) {
     if (!props.valid) {
-      return(
-        <div className={classes.error_msg}>
-            <p>{props.message}</p>
-        </div>
-      )
+        return (
+            <div className={classes.error_msg}>
+                <p>{props.message}</p>
+            </div>
+        );
     }
     return null;
-  }
+}
 
 const BookTrial = (props) => {
-    const kidlogo = process.env.PUBLIC_URL + '/images/query.svg';
-    const [name,setname] = useState('');
-    const [nameValid,setNameValid] = useState(false);
-    const [email,setemail] = useState('');
-    const [emailValid,setEmailValid] = useState(false);
-    const [standard,setstandard] = useState('');
-    const [standardValid,setStandardValid] = useState(false);
-    const [phone,setphone] = useState('');
-    const [phoneValid,setPhoneValid] = useState(false);
-    const [formValid,setFormValid] = useState(false);
-    const [errorMessage,setErrorMessage] = useState({});
+    const kidlogo = process.env.PUBLIC_URL + "/images/query.svg";
+    const [name, setname] = useState("");
+    const [nameValid, setNameValid] = useState(false);
+    const [email, setemail] = useState("");
+    const [emailValid, setEmailValid] = useState(false);
+    const [standard, setstandard] = useState("");
+    const [standardValid, setStandardValid] = useState(false);
+    const [phone, setphone] = useState("");
+    const [phoneValid, setPhoneValid] = useState(false);
+    const [formValid, setFormValid] = useState(false);
+    const [errorMessage, setErrorMessage] = useState({});
 
-    const validateForm = ()=>{
+    const validateForm = () => {
         setFormValid(nameValid && emailValid && standardValid && phoneValid);
-    }
+    };
     const updateName = (data) => {
-        let errorMsg = {...errorMessage};
+        let errorMsg = { ...errorMessage };
         setname(data);
-        if(data.length > 0){
+        if (data.length > 0) {
             setNameValid(true);
-        }else{
+        } else {
             setNameValid(false);
-            errorMsg.name = 'Name is required!';
+            errorMsg.name = "Name is required!";
             setErrorMessage(errorMsg);
         }
         validateForm();
-    }
+    };
 
     const updateEmail = (data) => {
-        let errorMsg = {...errorMessage};
+        let errorMsg = { ...errorMessage };
         setemail(data);
-        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
             setEmailValid(false);
-            errorMsg.email = 'Invalid email format';
+            errorMsg.email = "Invalid email format";
             setErrorMessage(errorMsg);
-        }else{
+        } else {
             setEmailValid(true);
         }
         validateForm();
-    }
+    };
 
     const updateStandard = (data) => {
-        let errorMsg = {...errorMessage};
+        let errorMsg = { ...errorMessage };
         setstandard(data);
-        if(data.length > 0){
+        if (data.length > 0) {
             setStandardValid(true);
-        }else{
+        } else {
             setStandardValid(false);
-            errorMsg.standard = 'Class is required!';
+            errorMsg.standard = "Class is required!";
             setErrorMessage(errorMsg);
         }
         validateForm();
-    }
+    };
 
     const updatePhone = (data) => {
-        let errorMsg = {...errorMessage};
+        let errorMsg = { ...errorMessage };
         setphone(data);
-        if(data.length === 10){
+        if (data.length === 10) {
             setPhoneValid(true);
-        }else{
+        } else {
             setPhoneValid(false);
-            errorMsg.phone = '10 digit phone number is required';
+            errorMsg.phone = "10 digit phone number is required";
             setErrorMessage(errorMsg);
         }
         validateForm();
-    }
+    };
 
     return (
         <div className={classes.main}>
-            <div className={classes.container} style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/images/CurveBackground.svg'})`}}>
+            <div
+                className={classes.container}
+                style={{
+                    backgroundImage: `url(${
+                        process.env.PUBLIC_URL + "/images/CurveBackground.svg"
+                    })`,
+                }}
+            >
                 <div className={classes.item1}>
                     <h2>Ask Query</h2>
-                    <p>Do you have any question regarding any Events or anything to ask from the Event Manager?</p>
-                    <img src={kidlogo} alt='creative'></img>
+                    <p>
+                        Do you have any question regarding any Events or
+                        anything to ask from the Event Manager?
+                    </p>
+                    <img src={kidlogo} alt="creative"></img>
                 </div>
                 <div className={classes.item2}>
-                    <form action='#' id='js-form' className={classes.form}>
+                    <form action="#" id="js-form" className={classes.form}>
                         <div className={classes.form_group}>
-                            <label className={classes.label} htmlFor='username'>Query</label>
-                            < ValidationMessage valid={nameValid} message={errorMessage.name} />
-                            <textarea type='text' id='username' name='username' className={classes.form_field}
-                            value={name} onChange={(e) => updateName(e.target.value)} rows="5" column="10" placeholder='Enter your query here'/>
+                            <label className={classes.label} htmlFor="username">
+                                Query
+                            </label>
+                            <ValidationMessage
+                                valid={nameValid}
+                                message={errorMessage.name}
+                            />
+                            <textarea
+                                type="text"
+                                id="username"
+                                name="username"
+                                className={classes.form_field}
+                                value={name}
+                                onChange={(e) => updateName(e.target.value)}
+                                rows="5"
+                                column="10"
+                                placeholder="Enter your query here"
+                            />
                         </div>
                         {/* <div className={classes.form_group}>
                             <label className={classes.label} htmlFor='email'>Email</label>
@@ -102,7 +126,7 @@ const BookTrial = (props) => {
                             <input type='email' id='email' name='email' className={classes.form_field}
                             value={email} onChange={(e) => updateEmail(e.target.value)} placeholder='Enter email'/>
                         </div>
-                        <div className={classes.phoneclass}>
+                        <div className={classes.rollnoclass}>
                             <div className={classes.form_group1}>
                                 <label className={classes.label} htmlFor='phone'>Phone</label>
                                 < ValidationMessage valid={phoneValid} message={errorMessage.phone} />
@@ -116,13 +140,19 @@ const BookTrial = (props) => {
                                 value={standard} onChange={(e) => updateStandard(e.target.value)} placeholder='class'/>
                             </div>
                         </div> */}
-                        <div className='form-controls'>
-                            <button className={classes.btn} type='submit' disabled={!formValid}>Ask Query</button>
+                        <div className="form-controls">
+                            <button
+                                className={classes.btn}
+                                type="submit"
+                                disabled={!formValid}
+                            >
+                                Ask Query
+                            </button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 export default BookTrial;
--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,180 +1,311 @@
-import React,{useState} from 'react';
-import classes from './Login.module.css';
-
+import React, { useState } from "react";
+import classes from "./Login.module.css";
 function ValidationMessage(props) {
     if (!props.valid) {
-      return(
-        <div className={classes.error_msg}>
-            <p>{props.message}</p>
-        </div>
-      )
+        return (
+            <div className={classes.error_msg}>
+                <p>{props.message}</p>
+            </div>
+        );
     }
     return null;
-  }
+}
 
 const SignUp = (props) => {
-    const [name,setname] = useState('');
-    const [nameValid,setNameValid] = useState(false);
-    const [email,setemail] = useState('');
-    const [emailValid,setEmailValid] = useState(false);
-    const [standard,setstandard] = useState('');
-    const [standardValid,setStandardValid] = useState(false);
-    const [phone,setphone] = useState('');
-    const [phoneValid,setPhoneValid] = useState(false);
-    const [password,setpassword] = useState('');
-    const [passwordValid,setPasswordValid] = useState(false);
-    const [cnfpassword,setcnfpassword] = useState('');
-    const [cnfpasswordValid,setCnfPasswordValid] = useState(false);
-    const [formValid,setFormValid] = useState(false);
-    const [errorMessage,setErrorMessage] = useState({});
+    const [name, setname] = useState("");
+    const [nameValid, setNameValid] = useState(false);
+    const [email, setemail] = useState("");
+    const [emailValid, setEmailValid] = useState(false);
+    const [college, setcollege] = useState("");
+    const [collegeValid, setCollegeValid] = useState(false);
+    const [rollno, setrollno] = useState("");
+    const [rollnoValid, setRollnoValid] = useState(false);
+    const [password, setpassword] = useState("");
+    const [passwordValid, setPasswordValid] = useState(false);
+    const [cnfpassword, setcnfpassword] = useState("");
+    const [cnfpasswordValid, setCnfPasswordValid] = useState(false);
+    const [formValid, setFormValid] = useState(false);
+    const [errorMessage, setErrorMessage] = useState({});
+    const [loading, setLoading] = useState(false);
 
-    const validateForm = ()=>{
-        setFormValid(nameValid && emailValid && standardValid && phoneValid && passwordValid && cnfpasswordValid);
-    }
-    const updateName = (data) => {
-        let errorMsg = {...errorMessage};
-        setname(data);
-        if(data.length > 0){
-            setNameValid(true);
-        }else{
-            setNameValid(false);
-            errorMsg.name = 'Name is required!';
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let errorMsg = { ...errorMessage };
+        validateForm();
+        if (formValid) {
+            try {
+                setLoading(true);
+            } catch {
+                setFormValid(false);
+                errorMsg.name = "Faild to create an account";
+                setErrorMessage(errorMsg);
+            }
+            setLoading(false);
+        } else {
+            setFormValid(false);
+            errorMsg.name = "Form is invalid";
             setErrorMessage(errorMsg);
         }
-        validateForm();
-    }
+    };
+
+    const validateForm = () => {
+        updateName(name);
+        updateEmail(email);
+        updateCollege(college);
+        updateRollno(rollno);
+        updatePassword(password);
+        updateCnfPassword(cnfpassword);
+        if (
+            nameValid &&
+            emailValid &&
+            collegeValid &&
+            rollnoValid &&
+            passwordValid &&
+            cnfpasswordValid
+        ) {
+            console.log("x");
+            setFormValid(true);
+            console.log(
+                "Form = ",
+                formValid,
+                nameValid &&
+                    emailValid &&
+                    collegeValid &&
+                    rollnoValid &&
+                    passwordValid &&
+                    cnfpasswordValid
+            );
+        } else {
+            console.log("y");
+            setFormValid(false);
+        }
+    };
+    const updateName = (data) => {
+        let errorMsg = { ...errorMessage };
+        setname(data);
+        if (data.length > 0) {
+            setNameValid(true);
+        } else {
+            setNameValid(false);
+            errorMsg.name = "Name is required!";
+            setErrorMessage(errorMsg);
+        }
+        console.log("Name = ", nameValid);
+    };
 
     const updateEmail = (data) => {
-        let errorMsg = {...errorMessage};
+        let errorMsg = { ...errorMessage };
         setemail(data);
-        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
             setEmailValid(false);
-            errorMsg.email = 'Invalid email format';
+            errorMsg.email = "Invalid email format";
             setErrorMessage(errorMsg);
-        }else{
+        } else {
             setEmailValid(true);
         }
-        validateForm();
-    }
+        console.log("Email = ", emailValid);
+    };
 
-    const updateStandard = (data) => {
-        let errorMsg = {...errorMessage};
-        setstandard(data);
-        if(data.length > 0){
-            setStandardValid(true);
-        }else{
-            setStandardValid(false);
-            errorMsg.standard = 'Class is required!';
+    const updateCollege = (data) => {
+        let errorMsg = { ...errorMessage };
+        setcollege(data);
+        if (data.length > 0) {
+            setCollegeValid(true);
+        } else {
+            setCollegeValid(false);
+            errorMsg.name = "College Name is required!";
             setErrorMessage(errorMsg);
         }
-        validateForm();
-    }
+        console.log("College = ", collegeValid);
+    };
 
-    const updatePhone = (data) => {
-        let errorMsg = {...errorMessage};
-        setphone(data);
-        if(data.length === 10){
-            setPhoneValid(true);
-        }else{
-            setPhoneValid(false);
-            errorMsg.phone = '10 digit phone number is required';
+    const updateRollno = (data) => {
+        let errorMsg = { ...errorMessage };
+        setrollno(data);
+        if (data.length > 0) {
+            setRollnoValid(true);
+        } else {
+            setRollnoValid(false);
+            errorMsg.phone = "Roll No is required";
             setErrorMessage(errorMsg);
         }
-        validateForm();
-    }
+        console.log("Rollno = ", rollnoValid);
+    };
 
     const updatePassword = (data) => {
-        let errorMsg = {...errorMessage};
+        let errorMsg = { ...errorMessage };
         setpassword(data);
-        if(data.length === 0){
+        if (data.length === 0) {
             setPasswordValid(false);
-            errorMsg.password = 'Password is required';
-        }else if (data.length < 6){
+            errorMsg.password = "Password is required";
+        } else if (data.length < 6) {
             setPasswordValid(false);
-            errorMsg.password = 'Password needs to be 6 characters or more';
-        }else{
+            errorMsg.password = "Password needs to be 6 characters or more";
+        } else {
             setPasswordValid(true);
-            if(cnfpassword === password){
+            if (cnfpassword === password) {
                 setCnfPasswordValid(true);
             }
         }
         setErrorMessage(errorMsg);
-    }
+        console.log("Password = ", passwordValid);
+    };
     const updateCnfPassword = (data) => {
-        let errorMsg = {...errorMessage};
+        let errorMsg = { ...errorMessage };
         setcnfpassword(data);
-        if(data.length === 0){
+        if (data.length === 0) {
             setCnfPasswordValid(false);
-            errorMsg.cnfpassword = 'Confirm Password is required';
-        }else if (data !== password) {
-            setCnfPasswordValid(false)
-            errorMsg.cnfpassword = 'Password Mismatch';
-        }else{
+            errorMsg.cnfpassword = "Confirm Password is required";
+        } else if (data !== password) {
+            setCnfPasswordValid(false);
+            errorMsg.cnfpassword = "Password Mismatch";
+        } else {
             setCnfPasswordValid(true);
         }
         setErrorMessage(errorMsg);
-    }
+        console.log("Cnfpassword = ", cnfpasswordValid);
+    };
     return (
         <div className={classes.container}>
+            {/* Form Heading */}
             <div className={classes.heading}>
                 <h2>Sign Up</h2>
-                <h4>Get started with us today! Create your account by filling out the information below.</h4>
+                <h4>Register as a participant to participate in Inter-IIT</h4>
             </div>
-            <form action='#' id='js-form' className={classes.form}>
+            <ValidationMessage valid={formValid} message={errorMessage.name} />
+            <form
+                action="#"
+                id="js-form"
+                className={classes.form}
+                onSubmit={(e) => handleSubmit(e)}
+            >
+                {/* Participant Name field */}
                 <div className={classes.form_group}>
-                    <label className={classes.label} htmlFor='username'>Student Name</label>
-                    < ValidationMessage valid={nameValid} message={errorMessage.name} />
-                    <input type='text' id='username' name='username' className={classes.form_field}
-                    value={name} onChange={(e) => updateName(e.target.value)} placeholder='Enter student name'/>
+                    <label className={classes.label} htmlFor="username">
+                        Participant Name
+                    </label>
+                    <ValidationMessage
+                        valid={nameValid}
+                        message={errorMessage.name}
+                    />
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        className={classes.form_field}
+                        value={name}
+                        onChange={(e) => updateName(e.target.value)}
+                        placeholder="Enter participant name"
+                    />
                 </div>
-
+                {/* Email field */}
                 <div className={classes.form_group}>
-                    <label className={classes.label} htmlFor='email'>Email</label>
-                    < ValidationMessage valid={emailValid} message={errorMessage.email} />
-                    <input type='email' id='email' name='email' className={classes.form_field}
-                    value={email} onChange={(e) => updateEmail(e.target.value)} placeholder='Enter email'/>
+                    <label className={classes.label} htmlFor="email">
+                        Email
+                    </label>
+                    <ValidationMessage
+                        valid={emailValid}
+                        message={errorMessage.email}
+                    />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className={classes.form_field}
+                        value={email}
+                        onChange={(e) => updateEmail(e.target.value)}
+                        placeholder="Enter email"
+                    />
                 </div>
-
+                {/* College Name field */}
                 <div className={classes.form_group}>
-                    <label className={classes.label} htmlFor='CollageName'>Collage Name</label>
-                    < ValidationMessage valid={nameValid} message={errorMessage.name} />
-                    <input type='text' id='CollageName' name='CollageName' className={classes.form_field}
-                    value={name} onChange={(e) => updateName(e.target.value)} placeholder='Enter student name'/>
+                    <label className={classes.label} htmlFor="collegeName">
+                        college Name
+                    </label>
+                    <ValidationMessage
+                        valid={collegeValid}
+                        message={errorMessage.college}
+                    />
+                    <input
+                        type="text"
+                        id="collegeName"
+                        name="collegeName"
+                        className={classes.form_field}
+                        value={college}
+                        onChange={(e) => updateCollege(e.target.value)}
+                        placeholder="Enter college name"
+                    />
                 </div>
-
-                <div className={classes.phoneclass}>
-                    <div className={classes.form_group1}>
-                        <label className={classes.label} htmlFor='phone'>Roll Number</label>
-                        < ValidationMessage valid={phoneValid} message={errorMessage.phone} />
-                        <input type='phone' id='phone' name='phone' className={classes.form_field} 
-                        value={phone} onChange={(e) => updatePhone(e.target.value)} placeholder='Enter Phone Number'/>
-                    </div>
-                    <div className={classes.form_group2}>
-                        <label className={classes.label} htmlFor='standard'>year</label>
-                        < ValidationMessage valid={standardValid} message={errorMessage.standard}/>
-                        <input type='text' id='standard' name='standard' className={classes.form_field}
-                        value={standard} onChange={(e) => updateStandard(e.target.value)} placeholder='class'/>
-                    </div>
-                </div>
+                {/* Roll No field */}
                 <div className={classes.form_group}>
-                    <label className={classes.label} htmlFor='password'>Password</label>
-                    < ValidationMessage valid={passwordValid} message={errorMessage.password} />
-                    <input type='password' id='password' name='password' className={classes.form_field}
-                    value={password} onChange={(e) => updatePassword(e.target.value)} placeholder='Password'/>
+                    <label className={classes.label} htmlFor="rollno">
+                        Roll Number
+                    </label>
+                    <ValidationMessage
+                        valid={rollnoValid}
+                        message={errorMessage.rollno}
+                    />
+                    <input
+                        type="number"
+                        id="rollno"
+                        name="rollno"
+                        className={classes.form_field}
+                        value={rollno}
+                        onChange={(e) => updateRollno(e.target.value)}
+                        placeholder="Enter Roll Number"
+                    />
                 </div>
+                {/* Password */}
                 <div className={classes.form_group}>
-                    <label className={classes.label} htmlFor='password'>Confirm Password</label>
-                    < ValidationMessage valid={cnfpasswordValid} message={errorMessage.cnfpassword}/>
-                    <input type='password' id='confirm_password' name='cnfpassword' className={classes.form_field}
-                    value={cnfpassword} onChange={(e) => updateCnfPassword(e.target.value)} placeholder='Confirm Password'/>
+                    <label className={classes.label} htmlFor="password">
+                        Password
+                    </label>
+                    <ValidationMessage
+                        valid={passwordValid}
+                        message={errorMessage.password}
+                    />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        className={classes.form_field}
+                        value={password}
+                        onChange={(e) => updatePassword(e.target.value)}
+                        placeholder="Password"
+                    />
                 </div>
-                <div className='form-controls'>
-                    <button className={classes.btn} type='submit' disabled={!formValid}>Register</button>
+                {/* Confirm Password */}
+                <div className={classes.form_group}>
+                    <label className={classes.label} htmlFor="password">
+                        Confirm Password
+                    </label>
+                    <ValidationMessage
+                        valid={cnfpasswordValid}
+                        message={errorMessage.cnfpassword}
+                    />
+                    <input
+                        type="password"
+                        id="confirm_password"
+                        name="cnfpassword"
+                        className={classes.form_field}
+                        value={cnfpassword}
+                        onChange={(e) => updateCnfPassword(e.target.value)}
+                        placeholder="Confirm Password"
+                    />
+                </div>
+                {/* Submit */}
+                <div className="form-controls">
+                    <button
+                        className={classes.btn}
+                        type="submit"
+                        disabled={loading}
+                    >
+                        Register
+                    </button>
                 </div>
             </form>
-                
         </div>
     );
-}
+};
 
 export default SignUp;
