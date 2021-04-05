0826d38c47de98b96f1126b78a6199918d3e7b57
Bhasker Goel <bhasker2000goel@gmail.com>
2021-04-05 20:12:48 +0530

    Minor changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,25 +8,10 @@ import Event from "./EventsPage/EventsPage";
 import { Component } from "react";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { apiResponse: "" };
-    }
-
-    callAPI() {
-        fetch("http://localhost:5000/testAPI")
-            .then((res) => res.text())
-            .then((res) => this.setState({ apiResponse: res }));
-    }
-
-    componentWillMount() {
-        this.callAPI();
-    }
 
     render() {
         return (
             <Router>
-                <p>{this.state.apiResponse}</p>
                 <Navbar />
                 <Switch>
                     <Route path="/" exact component={Home} />
