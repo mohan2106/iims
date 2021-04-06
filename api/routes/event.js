88cebeebaf927de6df37e718627721266fe32cc0
Bhasker Goel <bhasker2000goel@gmail.com>
2021-04-07 02:04:45 +0530

    Added Scoreboard APIs

--- api/routes/event.js
+++ api/routes/event.js
@@ -17,16 +17,64 @@ router.get("/", async function (req, res) {
 });
 
 router.post("/", async function (req, res) {
-    const docRef = db.collection("event").doc();
-    await docRef
+    const eventRef = db.collection("event").doc();
+    const scoreboardRef = db.collection("scoreboard").doc(eventRef.id);
+    await eventRef
         .set(req.body)
-        .then(() => {
-            res.send("Event Created Successfully");
+        .then(async () => {
+            scores = [];
+            req.body.participatingCollege.forEach((college) => {
+                scores.push({
+                    college: college,
+                    score: 0,
+                });
+            });
+            await scoreboardRef
+                .set({
+                    scores: scores,
+                })
+                .then(() => {
+                    res.send("Event Created Successfully");
+                })
+                .catch((err) => {
+                    res.send(err);
+                });
         })
         .catch((err) => {
-            res.send("error");
             res.send(err);
         });
+});
+
+router.get("/scoreboard", async function (req, res) {
+    const scoreboardDoc = db.collection("scoreboard");
+    const snapshot = await scoreboardDoc.get();
+    let scores = {};
+    if (!snapshot.empty) {
+        snapshot.forEach((doc) => {
+            let data = doc.data();
+            data.id = doc.id;
+            data.scores.forEach((element) => {
+                console.log(scores);
+                console.log(element.college);
+                if (scores.hasOwnProperty(element.college)) {
+                    scores[element.college] =
+                        scores[element.college] + element.score;
+                } else {
+                    scores[element.college] = element.score;
+                }
+            });
+        });
+    }
+
+    let scoreboard = [];
+    for (let [k, v] of Object.entries(scores)) {
+        scoreboard.push({
+            college: k,
+            score: v,
+        });
+    }
+
+    res.send(scoreboard);
 });
 
 router.get("/:eventID", async function (req, res) {
@@ -61,6 +109,32 @@ router.delete("/:eventID", async function (req, res) {
         .delete()
         .then(() => {
             res.send("Event Deleted Successfully");
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
+
+router.get("/:eventID/scoreboard", async function (req, res) {
+    const document = await db
+        .collection("scoreboard")
+        .doc(req.params["eventID"])
+        .get();
+    let data = document.data();
+    if (data !== undefined) {
+        data.id = document.id;
+    } else {
+        data = {};
+    }
+    res.send(data.scores);
+});
+
+router.patch("/:eventID/scoreboard", async function (req, res) {
+    const docRef = db.collection("scoreboard").doc(req.params["eventID"]);
+    await docRef
+        .update(req.body)
+        .then(() => {
+            res.send("Scoreboard Updated Successfully");
         })
         .catch((err) => {
             res.send(err);
